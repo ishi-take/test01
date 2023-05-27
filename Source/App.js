d6732c7934ae1cb6fc53bc1ec78dcf526938bbc0
ishi-take <t.ishikawa@hayakawa-t.co.jp>
2023-05-27 13:44:30 +0900

    app

--- Source/App.js
+++ Source/App.js
@@ -5,7 +5,7 @@ var point = viewer.entities;
 point.add({
     position:Cesium.Cartesian3.fromDegrees(-75.59777,40.03883),
     point:{
-        pixelSize:10,
+        pixelsize:10,
         color:Cesium.Color.YELLOW
     }
 });

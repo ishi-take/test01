923a08f770aca374a12f7e53a9ba5f949afb0de8
ishi-take <t.ishikawa@hayakawa-t.co.jp>
2023-05-27 13:06:25 +0900

    Create App.js

---
+++ Source/App.js
@@ -0,0 +1,11 @@
+var viewer = new Cesium.viewer('cesiumContainer')
+//add a point
+var point = viewer.entities;
+
+point.add({
+    position:Cesium.Cartesian3.fromDegrees(-75.59777,40.03883),
+    point:{
+        pixelSize:10,
+        color:Cesium.Color.YELLOW
+    }
+});

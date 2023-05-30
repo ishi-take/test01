c0589b83c0a9f49b0c34a963ea84eff0fd12c599
ishi-take <t.ishikawa@hayakawa-t.co.jp>
2023-05-30 18:16:42 +0900

    toyo

--- Apps/CesiumViewer/App.js
+++ Apps/CesiumViewer/App.js
@@ -2,9 +2,9 @@
 var viewer = new Cesium.Viewer("cesiumContainer");
 
       var point = viewer.entities.add({
-      name:"福井市", //レイヤ名
-      description:"ここは福井市です。",　//レイヤの説明
-        position : Cesium.Cartesian3.fromDegrees(136.223554,36.061957,0), //経度,緯度,高さ
+      name:"豊明市", //レイヤ名
+      description:"ここは豊明市です。",　//レイヤの説明
+        position : Cesium.Cartesian3.fromDegrees(137.01307,35.05388,0), //経度,緯度,高さ
         point : {
             pixelSize : 10, //ポイントのサイズ
             color : Cesium.Color.BLUE //ポイントの色
